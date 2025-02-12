ccff70b721cce81e6921173681cdc3e325dd3862
Swathi <swathi.rethinavel@gmail.com>
2025-02-12 22:26:12 +0530

    sub of two numbers

---
+++ src/Example2/Arithmetic.java
@@ -0,0 +1,19 @@
+package Example2;
+
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int a=100;
+        int b=29;
+        int c=a+b;
+        System.out.println("The sum of two numbers is "+c);
+
+
+        int d=19;
+        int e=9;
+        int f=d-e;
+        System.out.println("The sub of two numbers is "+f);
+
+    }
+}
